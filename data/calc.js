aa89362f5890cd4a1e8ca0d00d9fa7fc73ae9bd6
Nigel Babu <nigelbabu@gmail.com>
2013-02-27 01:00:57 +0530

    Now it works!

--- data/calc.js
+++ data/calc.js
@@ -1,16 +1,10 @@
-self.port.on('clicked', function(url) {
-    if (url.contains('toggl.com/report/show')) {
-        console.log('This is the right page');
-        ve = document.getElementsByClassName('task-duration minutes');
-        console.log(ve.length);
-        for (var i = 0; i < ve.length; i++) {
-            ve[i].classList.add('hidden');
-            console.log(i);
-        }
-        he = document.getElementsByClassName('task-duration decimal hidden');
-        for (var i = 0; i < he.length; i++) {
-            he[i].classList.remove('hidden');
-            console.log(i);
-        }
+if (document.URL.contains('toggl.com/report/show')) {
+    ve = document.getElementsByClassName('task-duration minutes');
+    for (var i = 0; i < ve.length; i++) {
+        ve[i].classList.toggle('hidden');
     }
-});
+    he = document.getElementsByClassName('task-duration decimal');
+    for (var i = 0; i < he.length; i++) {
+        he[i].classList.toggle('hidden');
+    }
+}
