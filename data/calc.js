56092322f0cc40ba2a1d314323c68a45d7f94608
Nigel Babu <nigelbabu@gmail.com>
2013-02-27 01:35:39 +0530

    Divide by 8 too! (Fixes #1)

--- data/calc.js
+++ data/calc.js
@@ -1,10 +1,14 @@
 if (document.URL.contains('toggl.com/report/show')) {
     ve = document.getElementsByClassName('task-duration minutes');
     for (var i = 0; i < ve.length; i++) {
-        ve[i].classList.toggle('hidden');
+        if (!ve[i].classList.contains('hidden')) {
+            ve[i].classList.add('hidden');
+        }
     }
-    he = document.getElementsByClassName('task-duration decimal');
+    he = document.getElementsByClassName('task-duration decimal hidden');
     for (var i = 0; i < he.length; i++) {
+        days = parseFloat(he[i].innerHTML) / 8;
+        he[i].innerHTML = days;
         he[i].classList.toggle('hidden');
     }
 }
